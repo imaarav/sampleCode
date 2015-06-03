3bc9856af5f5f022191a68383d36d107611fa7b2
aarav <aarav@nyu.edu>
2015-06-02 21:54:13 -0400

    object copy case

--- code.java
+++ code.java
@@ -1,8 +1,32 @@
-class code
+class DummyBean
 {
+	private String s;
 	
-	public static main (string[] args)
+	public void setDummy(String sc)
 	{
-		int i;
+		this.s = sc;
+	}
+	
+	public String getDummy()
+	{
+		return this.s;
+	}
+} 
+
+
+
+public class code
+{
+	public static void main (String[] args)
+	{
+		DummyBean dum = new DummyBean();
+		dum.setDummy("foo");
+		System.out.println(dum.getDummy()); // prints 'foo'
+
+		DummyBean dumtwo = dum;
+		System.out.println(dumtwo.getDummy()); // prints 'foo'
+
+		dumtwo.setDummy("bar");
+		System.out.println(dum.getDummy()); // prints 'bar' but it should print 'foo'
 	}
 }
