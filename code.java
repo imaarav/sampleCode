05b0d13f29b320d0870a279bf6bbb8a52a0337f8
aarav <aarav@nyu.edu>
2015-06-05 20:31:42 -0400

    Missing Numbers

--- code.java
+++ code.java
@@ -1,32 +1,52 @@
-class DummyBean
+/*arraylist capacity-size indexoutofbounderror not solved yet omkar d*/
+
+import java.util.*;
+import java.io.*;
+
+public class MissingNumbers
 {
-	private String s;
-	
-	public void setDummy(String sc)
+	public static void main(String[] args)
 	{
-		this.s = sc;
-	}
-	
-	public String getDummy()
-	{
-		return this.s;
-	}
-} 
-
-
-
-public class code
-{
-	public static void main (String[] args)
-	{
-		DummyBean dum = new DummyBean();
-		dum.setDummy("foo");
-		System.out.println(dum.getDummy()); // prints 'foo'
-
-		DummyBean dumtwo = dum;
-		System.out.println(dumtwo.getDummy()); // prints 'foo'
-
-		dumtwo.setDummy("bar");
-		System.out.println(dum.getDummy()); // prints 'bar' but it should print 'foo'
+		Scanner in = new Scanner(System.in);
+		int[] a1, a2;
+		int n1, n2;
+		int min,max;
+		ArrayList <Boolean> al;
+		
+		n1 = in.nextInt();
+		a1 = new int[n1];
+		for (int i=0; i<n1; i++)
+		{
+			a1[i] = in.nextInt();
+		}
+		
+		min = a1[n1-1];
+		max = a1[n1-1];
+		n2 = in.nextInt();
+		a2 = new int[n2];
+		for (int i=0; i<n2; i++)
+		{
+			a2[i] = in.nextInt();
+			if (a2[i]<min) min = a2[i];
+			if (max<a2[i]) max = a2[i];
+		}
+		
+		al = new ArrayList<Boolean>(max+1);
+		
+		for (int i=0; i<a2.length; i++)
+		{
+			al.add(a2[i], true);
+		}
+		
+		for(int i=0; i<a1.length; i++)
+		{
+			al.add(a1[i], false);
+		}
+		
+		for(int i=min; i<al.size(); i++)
+		{
+			if (al.get(i) == true) System.out.println(i);
+		}
+		
 	}
 }
